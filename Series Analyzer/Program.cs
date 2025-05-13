4f51d093ee2187d1117c5be00e5175391d33726c
Shlomo-Turjeman <shlomot575@gmail.com>
2025-05-13 17:12:58 +0300

    Update Program.cs

--- Series Analyzer/Program.cs
+++ Series Analyzer/Program.cs
@@ -151,11 +151,7 @@ namespace Series_Analyzer
 
             for (int i = len - 1; i >= 0; i--)
             {
-<<<<<<< HEAD
                 Console.Write(series[i] + " ");
-=======
-                Console.Write(sreies[i] + " ");
->>>>>>> 62cd83daa2d53f0eef26899921669d601b4808fd
             }
             Console.WriteLine("");
         }
@@ -316,8 +312,3 @@ namespace Series_Analyzer
         }
     }
 }
-<<<<<<< HEAD
-=======
- 
-  
->>>>>>> 62cd83daa2d53f0eef26899921669d601b4808fd
